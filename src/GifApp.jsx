742454146c439ead7b398d1a014cb788a0f45df7
Fredy <fredy.huachaca21@gmail.com>
2023-04-07 00:07:57 -0500

    Agregando lista de categorias

--- src/GifApp.jsx
+++ src/GifApp.jsx
@@ -1,8 +1,26 @@
+import { useState } from "react"
 
 export const GifApp = () => {
+
+  const [categories, setCategories] = useState(['One punch', 'Dragon ball'])
+  console.log(categories)
+
+  const onAddCategory = ( ) => {
+    setCategories([...categories, 'hola'])
+  }
+
   return (
     <>
     <h1>Gif App</h1>
+
+    <button onClick={onAddCategory}>Agregar</button>
+
+    <ol>
+      {categories.map( x => {
+        return <li key={x}>{x}</li>
+      })}
+    </ol>
+
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { GifApp } from './GifApp'
 
+import './styles.css';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <>
+    {/* Título */}
     <GifApp />
+    {/* Input */}
+
+    {/*Listado de gifs  */}
+
+    </>
   </React.StrictMode>,
 )
