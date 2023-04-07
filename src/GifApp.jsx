52ebb32e7fe7b41e0d47bcb959348e4027ec5277
Fredy <fredy.huachaca21@gmail.com>
2023-04-07 00:29:37 -0500

    Agregando componente AddCategory

--- src/GifApp.jsx
+++ src/GifApp.jsx
@@ -1,20 +1,29 @@
 import { useState } from "react"
 
+import { AddCategory } from "./components/AddCategory"
+
 export const GifApp = () => {
 
   const [categories, setCategories] = useState(['One punch', 'Dragon ball'])
   console.log(categories)
 
   const onAddCategory = ( ) => {
+    // setCategories([...categories, 'hola'])
     setCategories([...categories, 'hola'])
+    
   }
 
   return (
     <>
+    {/* Título */}
     <h1>Gif App</h1>
 
-    <button onClick={onAddCategory}>Agregar</button>
+    {/* Input categoria */}
+    <AddCategory/>
 
+    <button onClick={onAddCategory}>Agregar</button>
+    
+    {/* Lista de categorias */}
     <ol>
       {categories.map( x => {
         return <li key={x}>{x}</li>
